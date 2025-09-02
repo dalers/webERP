4c1e53ccc92fd61adaf4193be4e91f6d29ab6a7f
Dale Scott <dale@dalescott.net>
2025-09-02 17:30:04 -0600

    add install/remove PLM schema scripts

---
+++ sql/remove_plm_schema.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `stockmaster` DROP `itemnote`;
+DROP TABLE IF EXISTS `stockrefs`;
+DROP TABLE IF EXISTS `supplinecard`;
+DROP TABLE IF EXISTS `stockmfr`;
+DROP TABLE IF EXISTS `stockmfrpn`;
