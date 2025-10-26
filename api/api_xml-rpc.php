857a2777571e9c3cf9bc609db49b29d17eafef09
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-10-26 21:55:00 +0300

    test

--- api/api_xml-rpc.php
+++ api/api_xml-rpc.php
@@ -1,5 +1,7 @@
 <?php
-
+$path = __DIR__ . '/includes/api_xml-rpc_definition.php;
+echo 'the path is: '. $path;
+exit;
 /**
  * Entrypoint for all xml-rpc calls (the only file that actually has to be accessible from the web)
  */
