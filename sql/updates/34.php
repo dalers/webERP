cb7c04cf2719e61a2b2438f3c51f3915044159ed
Dale Scott <dale@dalescott.net>
2025-12-23 17:01:30 -0700

    add comment with purpose

--- sql/updates/34.php
+++ sql/updates/34.php
@@ -1,5 +1,9 @@
 <?php
 
+// switch to allow creating a WO for a manufactured stock item without a BOM
+// resolves issue "Work orders cannot be made for parts without BOMs"
+// https://github.com/timschofield/webERP/issues/793
+
 NewConfigValue('AllowWoNoBom', 0);
 
 if ($_SESSION['Updates']['Errors'] == 0) {
