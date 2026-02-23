27ae7098cdee9aca06cd2566e55da9e31916b425
Dale Scott <dale@dalescott.net>
2026-02-23 13:47:03 -0700

    all plm tables created except manufacturers

--- sql/updates/43.php
+++ sql/updates/43.php
@@ -7,7 +7,7 @@
 // - PLM Features https://github.com/timschofield/webERP/wiki/PLM-Features
 
 
-// 1. ADD stockmaster.notes FOR AD HOC STOCK ITEM NOTES
+// 1. ADD STOCK ITEM NOTES COLUMN stockmaster.notes
 //    - Notes column is intended for ad hoc notes about a stock item
 //    - Parts&Vendors allows "approximately 60K" char
 //    - resolves issue 592 "Schema is missing an Item "Notes" field" https://github.com/timschofield/webERP/issues/592
@@ -31,13 +31,15 @@
 AddColumn('notes', 'stockmaster', 'text', 'NULL', '', 'actualcost');
 
 
-// 2. ADD TABLE "stockfils" STOCK ITEM RELATED FILE/URL TABLE
+// 2. ADD TABLE STOCK ITEM RELATED FILE/URL TABLE stockfils
 //    - for file name with path OR URL
 //    - equivalent to P&V FIL table
 //    - Parts&Vendors allows ?? char TODO confirm max number of characters
 //    - has foreign key constraint to stockmaster.stockid
 
 // CreateTable($Table, $SQL)
+// - do not use CreateTable() - found to fail silently when utf8mb4 is specified
+//   (required to create fk constraint from filstockid to stockmaster.stockid)
 //CreateTable('stockfils', 'CREATE TABLE stockfils (
 //  filid INTEGER NOT NULL AUTO_INCREMENT, 
 //  filstockid VARCHAR(64),
@@ -49,8 +51,6 @@ AddColumn('notes', 'stockmaster', 'text', 'NULL', '', 'actualcost');
 //  INDEX (filstockid), 
 //  PRIMARY KEY (filid)
 //) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4');
-
-// use SQL directly because CreateTable() above silently fails
  $SQL = "CREATE TABLE stockfils (
   filid INTEGER NOT NULL AUTO_INCREMENT,
   filstockid VARCHAR(64),
@@ -65,44 +65,61 @@ AddColumn('notes', 'stockmaster', 'text', 'NULL', '', 'actualcost');
 $ErrMsg = __('Error creating stockfils table');
 $Result = DB_query($SQL, $ErrMsg);
 
-// foreign key constraint
+// foreign key constraints
 // AddConstraint($Table, $Constraint, $Field, $ReferenceTable, $ReferenceField)
 AddConstraint('stockfils', 'stockfils_ibfk_1', 'filstockid', 'stockmaster', 'stockid');
 
-// foreign key constraint equivalent direct SQL (not needed)
+// equivalent SQL to create foreign key constraints
 //$SQL = "ALTER TABLE stockfils ADD CONSTRAINT stockfils_ibfk_1 FOREIGN KEY (filstockid) REFERENCES stockmaster(stockid) ON DELETE RESTRICT ON UPDATE RESTRICT";
 //$ErrMsg = __('Error creating stockfils_ibfk_1 fk constraint');
 //$Result = DB_query($SQL, $ErrMsg);
 
 
-// 3. ADD "supplierlin" SUPPLIER LINE CARD
+// 3. ADD SUPPLIER LINE CARD TABLE supplierlin
 //    - manufacturers sold by a supplier
 //    - equivalent to P&V LIN table
 //    - TODO follow column naming and index convention in suppliers table for supplierlinsuid and supplierlinmfrid
     
 // CreateTable($Table, $SQL)
-//CreateTable('supplierlin', 'CREATE TABLE `supplierlin` (
-//  `supplierlinid` INTEGER NOT NULL AUTO_INCREMENT, 
-//  `supplierlinsuid` INTEGER NOT NULL DEFAULT 0,
-//  `supplierlinmfrid` INTEGER NOT NULL DEFAULT 0,
-//  INDEX (`supplierlinsuid`), 
-//  INDEX (`supplierlinmfrid`), 
-//  PRIMARY KEY (`supplierlinid`)
-//)');
+// - do not use CreateTable() - expected to fail silently when utf8mb4 is specified
+//CreateTable(supplierlin, 'CREATE TABLE supplierlin (
+//  supplierlinid INTEGER NOT NULL AUTO_INCREMENT, 
+//  supplierlinsuid INTEGER NOT NULL DEFAULT 0,
+//  supplierlinmfrid INTEGER NOT NULL DEFAULT 0,
+//  INDEX (supplierlinsuid), 
+//  INDEX (supplierlinmfrid), 
+//  PRIMARY KEY (supplierlinid)
+//) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4');
+$SQL = "CREATE TABLE supplierlin (
+  supplierlinid INTEGER NOT NULL AUTO_INCREMENT, 
+  supplierlinsuid INTEGER NOT NULL DEFAULT 0,
+  supplierlinmfrid INTEGER NOT NULL DEFAULT 0,
+  INDEX (supplierlinsuid), 
+  INDEX (supplierlinmfrid), 
+  PRIMARY KEY (supplierlinid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+$ErrMsg = __('Error creating supplierlin table');
+$Result = DB_query($SQL, $ErrMsg);
+
+// foreign key constraints
+// TODO investigate required fk constraints for supplierlin table
+// - is supplierlinsuid a fk for suppliers.supplierid?
+// - is supplierlinmfrid a fk for manufacturers.manufacturers_id?
 
 
-// 4. ADD "manufacturers" TABLE
+// 4. ADD MANUFACTURERS TABLE manufacturers
 //    - manufacturers for specifying the OEM for an item purchased from a supplier
 //    - equivalent to P&V MFR table
 //      - Parts&Vendors allows ?? char TODO confirm number of characters
 //    - TODO follow naming and index convention in suppliers table
 //
-//    The "manufacturers" table currently exists for storing "brands" (used for sales
-//    reporting IIUC). The current table must be renamed first in a previous db update
-//    (table must be renamed first in a new preceeding db update and all code references
-//    revised accordingly at the same time.
+//    TODO change name of current "manufacturers" table and update all code references
+//    The "manufacturers" table already exists for storing "brands" (used for
+//    sales reporting IIUC). The current table must be renamed first in a seperate
+//    db update and all code references revised accordingly at the same time.
 
 // CreateTable($Table, $SQL)
+// - do not use CreateTable() - expected to fail silently when utf8mb4 is specified
 //CreateTable('manufacturers', 'CREATE TABLE `manufacturers` (
 //  `manufacturersid` INTEGER NOT NULL AUTO_INCREMENT,
 //  `manufacturersname` VARCHAR(50) NOT NULL, 
@@ -123,6 +140,31 @@ AddConstraint('stockfils', 'stockfils_ibfk_1', 'filstockid', 'stockmaster', 'sto
 //  UNIQUE (`manufacturersname`), 
 //  PRIMARY KEY (`manufacturersid`)
 //)');
+//$SQL = "CREATE TABLE manufacturers (
+//  manufacturersid INTEGER NOT NULL AUTO_INCREMENT,
+//  manufacturersname VARCHAR(50) NOT NULL, 
+//  manufacturersaddress VARCHAR(255), 
+//  manufacturerscountry VARCHAR(50), 
+//  manufacturerscontact1 VARCHAR(50), 
+//  manufacturerscontact2 VARCHAR(50), 
+//  manufacturersphone1 VARCHAR(20), 
+//  manufacturersphone2 VARCHAR(20), 
+//  manufacturersfax VARCHAR(20), 
+//  manufacturersweb VARCHAR(255), 
+//  manufacturersnotes LONGTEXT, 
+//  manufacturerscode VARCHAR(20), 
+//  manufacturersmail1 VARCHAR(50), 
+//  manufacturersmail2 VARCHAR(50), 
+//  manufacturersnophoneprefix` TINYINT(1) DEFAULT 0, 
+//  INDEX (`manufacturersid`), 
+//  UNIQUE (`manufacturersname`), 
+//  PRIMARY KEY (`manufacturersid`)
+//) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+//$ErrMsg = __('Error creating manufacturers table');
+//$Result = DB_query($SQL, $ErrMsg);
+
+// foreign key constraints
+// - table is assumed to be master data and has no fk constraints
 
 
 // 5. ADD "manufacturerspn" MANUFACTURERS PART NUMBER TABLE
@@ -133,14 +175,32 @@ AddConstraint('stockfils', 'stockfils_ibfk_1', 'filstockid', 'stockmaster', 'sto
 //    - TODO confirm if manufacturerspnid is a child foreign key for stockmaster.stockid (if so add constraint)
 
 // CreateTable($Table, $SQL)
+// - do not use CreateTable() - expected to fail silently when utf8mb4 is specified
 //CreateTable('manufacturerspn', 'CREATE TABLE `manufacturerspn` (
 //  `manufacturerspnid` INTEGER NOT NULL AUTO_INCREMENT,
 //  `manufacturerspnmfrid` INTEGER DEFAULT 0, 
 //  `manufacturerspnpnpart` VARCHAR(50), 
-//  INDEX (`manufacturerspnmfrid`), 
+//  INDEX (`manufacturerspnmfrid`),
 //  INDEX (`manufacturerspnpnpart`),
 //  PRIMARY KEY (`manufacturerspnid`)
 //)');
+$SQL = "CREATE TABLE manufacturerspn (
+  manufacturerspnid INTEGER NOT NULL AUTO_INCREMENT,
+  manufacturerspnmfrid INTEGER DEFAULT 0, 
+  manufacturerspnpnpart VARCHAR(50), 
+  INDEX (manufacturerspnmfrid), 
+  INDEX (manufacturerspnpnpart),
+  PRIMARY KEY (manufacturerspnid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+$ErrMsg = __('Error creating manufacturerspn table');
+$Result = DB_query($SQL, $ErrMsg);
+
+// TODO CHANGE mfnufacturerspnid TO VARCHAR(64) IF FK TO STOCKMASTER.STOCKID
+
+// foreign key constraints
+// TODO add foreign key constraints for manufacturerspn table
+//  - is manufacturerspnid a fk to ??? TODO change column type to same as webERP parent column
+//  - is manufacturerspnmfrid a fk to stockmaster.stockid? TODO change column type to VARCHAR(64)
 
 
 // TODO ADD BOM IMPORT (JUST A REMINDER - NOTHING TO DO WITH UPGRADE) 
