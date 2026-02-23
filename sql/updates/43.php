07a3a57dc8a8350f281886b46e23409a326ff7f8
Dale Scott <dale@dalescott.net>
2026-02-23 08:47:22 -0700

    direct SQL required only to create stockfils table

--- sql/updates/43.php
+++ sql/updates/43.php
@@ -38,25 +38,20 @@ AddColumn('notes', 'stockmaster', 'text', 'NULL', '', 'actualcost');
 //    - has foreign key constraint to stockmaster.stockid
 
 // CreateTable($Table, $SQL)
-//CreateTable('stockfils', 'CREATE TABLE `stockfils` (
-//  `filid` INTEGER NOT NULL AUTO_INCREMENT, 
-//  `filstockid` VARCHAR(64),
-//  `filfilepath` VARCHAR(255), 
-//  `filefilename` VARCHAR(255), 
-//  `filview` TINYINT(1) DEFAULT 0, 
-//  `filnotes` VARCHAR(50), 
-//  INDEX (`filid`, `filstockid`), 
-//  INDEX (`filstockid`), 
-//  PRIMARY KEY (`filid`)
+//CreateTable('stockfils', 'CREATE TABLE stockfils (
+//  filid INTEGER NOT NULL AUTO_INCREMENT, 
+//  filstockid VARCHAR(64),
+//  filfilepath VARCHAR(255), 
+//  filefilename VARCHAR(255), 
+//  filview TINYINT(1) DEFAULT 0,
+//  filnotes VARCHAR(50), 
+//  INDEX (filid, filstockid), 
+//  INDEX (filstockid), 
+//  PRIMARY KEY (filid)
 //) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4');
 
-// foreign key constraint
-// AddConstraint($Table, $Constraint, $Field, $ReferenceTable, $ReferenceField)
-//AddConstraint('stockfils', 'stockfils_ibfk_1', 'filstockid', 'stockmaster', 'stockid');
-
-// must use SQL directly due to CreateTable() error
- 
-$SQL = "CREATE TABLE stockfils (
+// use SQL directly because CreateTable() above silently fails
+ $SQL = "CREATE TABLE stockfils (
   filid INTEGER NOT NULL AUTO_INCREMENT,
   filstockid VARCHAR(64),
   filfilepath VARCHAR(255),
@@ -70,9 +65,14 @@ $SQL = "CREATE TABLE stockfils (
 $ErrMsg = __('Error creating stockfils table');
 $Result = DB_query($SQL, $ErrMsg);
 
-$SQL = "ALTER TABLE stockfils ADD CONSTRAINT stockfils_ibfk_1 FOREIGN KEY (filstockid) REFERENCES stockmaster(stockid) ON DELETE RESTRICT ON UPDATE RESTRICT";
-$ErrMsg = __('Error creating stockfils_ibfk_1 fk constraint');
-$Result = DB_query($SQL, $ErrMsg);
+// foreign key constraint
+// AddConstraint($Table, $Constraint, $Field, $ReferenceTable, $ReferenceField)
+AddConstraint('stockfils', 'stockfils_ibfk_1', 'filstockid', 'stockmaster', 'stockid');
+
+// foreign key constraint equivalent direct SQL (not needed)
+//$SQL = "ALTER TABLE stockfils ADD CONSTRAINT stockfils_ibfk_1 FOREIGN KEY (filstockid) REFERENCES stockmaster(stockid) ON DELETE RESTRICT ON UPDATE RESTRICT";
+//$ErrMsg = __('Error creating stockfils_ibfk_1 fk constraint');
+//$Result = DB_query($SQL, $ErrMsg);
 
 
 // 3. ADD "supplierlin" SUPPLIER LINE CARD
