2397f0ac375c66afebb2302f539a007f7a8038d4
Dale Scott <dale@dalescott.net>
2026-08-17 14:52:08 -0600

    Correct description in update

--- sql/updates/45.php
+++ sql/updates/45.php
@@ -3,4 +3,4 @@
 ChangeColumnSize('stockact', 'stockcategory', 'VARCHAR(20)', ' NOT NULL ', '', '20');
 ChangeColumnSize('stockact', 'lastcostrollup', 'VARCHAR(20)', ' NOT NULL ', '', '20');
 
-UpdateDBNo(basename(__FILE__, '.php'), __('New table for stock item notes'));
+UpdateDBNo(basename(__FILE__, '.php'), __('Correct stock category GL account code column size to correct over-expanding in update 43'));
